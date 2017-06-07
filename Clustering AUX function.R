b23442993c60e743c27981e47900071164b3fa9f
Daniel Li <Daniel.Li@Finity.local>
2017-06-07 17:13:36 +1000

    added stoping condition and

--- Clustering AUX function.R
+++ Clustering AUX function.R
@@ -1,5 +1,5 @@
 
-#-------------asdfasfsdf----------------------asdfasdfasf------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
 #----------------------------function to perform cut for each data point of a dimention---------------------------------
 #-----------------------------------------------------------------------------------------------------------------------
 cut <- function(cutdata){
@@ -43,64 +43,96 @@ cut <- function(cutdata){
                     info.gain[i] <- 1 - infox 
           }
           # cut 1
-          d_cut <- as.numeric(cutdata[which(info.gain == max(info.gain,na.rm = T))])
+          d_cut <- as.numeric(cutdata[which(info.gain == max(info.gain,na.rm = T))])[1]
           return(d_cut)
 }
 
 
+
 #-----------------------------------------------------------------------------------------------------------------------
-#----------------------------function to return the best cut for a dimention---------------------------------
+#----------------------------function to return the best cut for a dimention and the 2 step ahead cut-------------------
 #-----------------------------------------------------------------------------------------------------------------------
 
-getCut <- function(data,feature){
-          #t_data <- clusterdata[,"X2"]
-          t_data <- data[,feature]
-          total.num<- length(t_data)
+getCut <- function(data){
+          #data <- test[,1]
+          
+          total.num<- length(data)
           #for know dimension each value calc the proportion of data by split
-          Range <- max(t_data) - min(t_data)
+          Range <- max(data) - min(data)
           #get best cut 1
-          dcut1 <- cut(t_data)
+          dcut1 <- cut(data)
           # right of cut1 region get next best cut 
-          cutdir <- density_dir(t_data,dcut1)
+          cutdir <- density_dir(data,dcut1)
           
           if(cutdir == "Left"){
-                    temp <- t_data[t_data < as.numeric(dcut1)]}else{
-                              temp <- t_data[t_data > as.numeric(dcut1)]
+                    temp <- data[data < as.numeric(dcut1)]}else{
+                              temp <- data[data > as.numeric(dcut1)]
                     }
           
           dcut2 <- cut(temp)
           # calc density measure
-          
-          dcut1_dcut2_Y <-  length(temp[temp < dcut2]) 
-          dcut1_dcut2_N <-  total.num*(dcut2-dcut1)/Range  
-          
-          desity_cut12 <- dcut1_dcut2_Y / dcut1_dcut2_N
-          
-          dcut2_r_Y <-  length(temp[temp > dcut2]) 
-          dcut2_r_N <-  total.num*(max(t_data)-dcut2)/Range  
-          
-          desity_cut2r <- dcut2_r_Y / dcut2_r_N
-          
-          if(desity_cut2r < desity_cut12){ 
-                    # if the region between cut1 and cut2 denser than that between cut2 and right boundary then cut2 is final
-                    cut <- dcut2} else{
-                              # if the region between cut1 and cut2 less dense than that between cut2 and right boundary then perform cut3 and cut3 is final
-                              temp1 <- temp[temp < dcut2]
-                              cut <- cut(temp1)
+          if(cutdir == "Left"){
+                    
+                    dcut1_dcut2_Y <-  length(temp[temp > dcut2]) 
+                    dcut1_dcut2_N <-  total.num*(abs(dcut2-dcut1))/Range  
+                    
+                    desity_cut12 <- dcut1_dcut2_Y / dcut1_dcut2_N
+                    
+                    dcut2_r_Y <-  length(temp[temp < dcut2]) 
+                    dcut2_r_N <-  total.num*(abs(dcut2 - min(data)))/Range  
+                    
+                    desity_cut2r <- dcut2_r_Y / dcut2_r_N
+                    
+                    if(desity_cut2r < desity_cut12){ 
+                              # if the region between cut1 and cut2 denser than that between cut2 and right boundary then cut2 is final
+                              bestcut <- dcut2
+                              final_N <- total.num*abs(bestcut - min(data))/Range
+                              final_Y <- length(data[data < bestcut])
+                              final.density <- final_Y/final_N} else{
+                                        # if the region between cut1 and cut2 less dense than that between cut2 and right boundary then perform cut3 and cut3 is final
+                                        temp1 <- temp[temp > dcut2]
+                                        bestcut <- cut(temp1)
+                                        final_N <- total.num*abs(bestcut - dcut1)/Range
+                                        final_Y <- length(data[data < dcut1 & data > bestcut])
+                                        final.density <- final_Y/final_N
+                              }
+          } else {
+                    dcut1_dcut2_Y <-  length(temp[temp < dcut2]) 
+                    dcut1_dcut2_N <-  total.num*(abs(dcut2-dcut1))/Range  
+                    
+                    desity_cut12 <- dcut1_dcut2_Y / dcut1_dcut2_N
+                    
+                    dcut2_r_Y <-  length(temp[temp > dcut2]) 
+                    dcut2_r_N <-  total.num*(abs(dcut2 - max(data)))/Range  
+                    
+                    desity_cut2r <- dcut2_r_Y / dcut2_r_N
+                    
+                    if(desity_cut2r < desity_cut12){ 
+                              # if the region between cut1 and cut2 denser than that between cut2 and right boundary then cut2 is final
+                              bestcut <- dcut2
+                              final_N <- total.num*abs(bestcut - max(data))/Range
+                              final_Y <- length(data[data > bestcut])
+                              final.density <- final_Y/final_N
+                              } else{
+                                        # if the region between cut1 and cut2 less dense than that between cut2 and right boundary then perform cut3 and cut3 is final
+                                        temp1 <- temp[temp < dcut2]
+                                        bestcut <- cut(temp1)
+                                        final_N <- total.num*abs(bestcut - dcut1)/Range
+                                        final_Y <- length(data[data > dcut1 & data < bestcut])
+                                        final.density <- final_Y/final_N
+                              }
+                    
                     }
-         return(cut)
+
+          # store result in a list -- bestcut, lhsdata, rhsdata
+          final.cut <- data.frame(bestcut,final.density)
+          return(final.cut)
           
 }
 
-#------------------------------------------------------------------------------
-# get the best cut from variable
-#------------------------------------------------------------------------------
-
-
-
 
 #-----------------------------------------------------------------------------------------
-#------------------------------ Density test
+#------------------------------ Density test make sure ahead looking cut only in low density regions
 #-----------------------------------------------------------------------------------------
 
 density_dir <- function(data,numcut){
@@ -118,25 +150,132 @@ density_dir <- function(data,numcut){
          return(direction)
 }
 
+#-----------------------------------------------------------------
+# get range
+#-----------------------------------------------------------------
+
+getRange <- function(data){
+          temp <- range(data)
+          temp <- temp[2] - temp[1]
+          return(temp)
+}
+
+#-----------------------------------------------------------------
+# get overall best cut
+#-----------------------------------------------------------------
+
+getFeatureCut <- function(data){
+          
+          features <- names(data)
+          
+          result <- list()
+          N <- nrow(data)
+          for (c in features){
+                    
+                    if (is.numeric(data[,c])){
+                              featuredata <- data[,c]
+                              result[[c]] <- getCut(featuredata)}
+                    else{
+                              next
+                    }
+          }
+          result <- do.call(rbind,result)
+          return(result)
+}
+
+cutResult <- getFeatureCut(test)
+
+#------------------------------------------------------------------------------
+# get the best cut from variable and apply to data, split data frame
+#------------------------------------------------------------------------------
+
+getBestCut<- function(data,cutResult){
+          cutResult$var<- rownames(cutResult)
+          out <- cutResult[cutResult$final.density == min(cutResult$final.density),]
+          var <- as.character(out$var)
+          cutpoint <- as.numeric(out$bestcut)
+          data[,paste0(var,"cut")] <- "left"
+          data[,paste0(var,"cut")][data[var] > cutpoint + 0.00001] <- "right"
+          
+          return(out)
+} 
+
+runCut(data)
 
 
+runCut <- function(data){
+          cutResult<- getFeatureCut(data)
+          output <- getBestCut(data,cutResult)
+          
+          return(output)
+}
 
+#-------------------------------------------------------------------------------
+#  function to grow tree
+#-------------------------------------------------------------------------------
 
+data <- test
+depth <- 3
+minobs <- 10
+growClust <- function(data ,depth, minobs){
+          numnodes <- 2^(depth + 1) - 1
+          # initialize trace matrix, result table
+          idx <- matrix(nrow = nrow(data),ncol = numnodes )
+          rtable <- data.frame(var = NA,bestcut=NA)
+          # root nodes
+          idx[,1] <-  rep(x = 1,nrow(data))
+          
+          # first cut
+          rtable$var[1] <- runCut(data)$var
+          rtable$bestcut[1]<- runCut(data)$bestcut
+          indexL <- 1*(data[,rtable[1,1]] < rtable[1,2]+ 0.00001 & as.logical(idx[,1]))
+          indexR <- 1*((data[,rtable[1,1]] > rtable[1,2]) & as.logical(idx[,1]))
+          idx[,2] <- indexL
+          idx[,3] <- indexR
+          
+          # build out tree
+          for(i in 2:(2^(depth)-1)){
 
-
-
-clusterdata <- data.frame(A$X)
-t_data <- clusterdata[,"X2"]
-names(clusterdata) <- c("X1","X2")
-x <- getCut(clusterdata,"X1")
-y <- getCut(clusterdata,"X2")
-
-
+                    
+                    # add min obs stoping 
+                    if (sum(idx[,i]) > minobs){
+                              tempdata <- data[as.logical(idx[,i]),]
+                              rtable[i,1] <- runCut(tempdata)$var
+                              rtable[i,2] <- runCut(tempdata)$bestcut
+                              indexL <- 1*(data[,rtable[i,1]] < rtable[i,2] & as.logical(idx[,i]))
+                              indexR <- 1*(!(data[,rtable[i,1]] < rtable[i,2]) & as.logical(idx[,i]))
+                              idx[,2*i] <- indexL
+                              idx[,2*i + 1] <- indexR          
+                    }else{
+                              rtable[i,1] <- "no cut"
+                              rtable[i,2] <- NA
+                              idx[,2*i] <- 0
+                              idx[,2*i + 1] <- 0
+                    }
+                    
+          }
+          return(rtable)
+}
 
 ggplot(clusterdata, aes(x = X1 , y =X2 )) + 
           geom_point()+
-          geom_vline(xintercept =  as.numeric(x),col ="blue") +
-          geom_hline(yintercept =  as.numeric(y),col = "purple")
+          geom_vline(xintercept =  as.numeric(0.2263293),col ="blue") +
+          geom_segment(x = -0.2 ,xend =0.2263293 ,y = 0.56587511,yend = 0.56587511 , col = "purple")+
+          geom_segment(x = 0.2263293 ,xend =1.2263293 ,y = 0.55646372,yend = 0.55646372 , col = "purple")+
+          geom_segment(x = -0.2 ,xend =0.2263293 ,y = 0.53092844,yend = 0.53092844 , col = "purple") +
+          geom_segment(x = -0.2 ,xend =0.2263293 ,y = 0.68425761,yend = 0.68425761 , col = "purple") +
+          geom_segment(x = 0.2263293 ,xend =1.2263293 ,y = 0.50764796,yend = 0.50764796 , col = "purple") +
+          geom_segment(x = 0.55860068 ,xend =0.55860068 ,y = 0.55646372,yend = 1 , col = "purple") +
+          
+          geom_segment(x = 0.2779978 ,xend =0.2779978 ,y = 0,yend = 0.5149316 , col = "purple")+
+          geom_segment(x = 0.3518865 ,xend =0.3518865 ,y = 0.5149316,yend = 1 , col = "purple")+
+          geom_vline(xintercept =  as.numeric(0.7411899),col ="blue")
+
+geom_vline(xintercept =  as.numeric(0.1582618),col ="blue") +
+          geom_vline(xintercept =  as.numeric(-0.03242239),col ="blue") +
+          geom_segment(x = 0.4853177,xend =1.1 ,y = 0.4658396,yend = 0.4658396 , col = "purple") + 
+          geom_segment(x = -0.03242239 ,xend =0.1821867 ,y = 0.5149316,yend = 0.5149316 , col = "purple")
 
 
-rm(dcut1)
+
+
--- R&D Clustering.R
+++ R&D Clustering.R
@@ -1,24 +1,16 @@
 library(ggplot2)
 library(clusterSim)
 library(data.table)
+library(MixSim)
 options(scipen = 100)
-grnd <- cluster.Gen(c(50,50), model=3, means=c(8,11,14),
-                    cov=c(1,1),numNoisyVar=1)
-clusterdata <- data.frame(grnd$data)
-clusterdata$X3 <- rnorm(n = 100,mean = 4,sd = 1)
-clusterdata$X4 <- abs(rexp(100))
-
-
-
 
 
 set.seed(1234)
-Q <- MixSim(BarOmega = 0.01, K = 4, p = 2)
-A <- simdataset(n = 500, Pi = Q$Pi, Mu = Q$Mu, S = Q$S, n.out = 10)
+Q <- MixSim(BarOmega = 0.01, K = 3, p = 2)
+A <- simdataset(n = 5000, Pi = Q$Pi, Mu = Q$Mu, S = Q$S, n.out = 10)
 colors <- c("red", "green", "blue", "brown", "magenta")
 
 par(mar = c(0.1, 0.1, 0.1, 0.1))
 plot(A$X, col = colors[A$id], pch = 19, cex = 0.8, xlab = "", ylab = "", axes = FALSE)
 box()
 
-library("MixSim")
